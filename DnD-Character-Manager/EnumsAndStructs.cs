ec23ff4ec1230b7ca170756e9a84769c78cbeb28
Scott Daniels <archerofyail@live.com>
2015-12-01 05:07:15 -0700

    Added Skill Struct, mainstat type enum, and switched main stats to dictionary

---
+++ DnD-Character-Manager/EnumsAndStructs.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace DnD_Character_Manager
+{
+
+	public enum MainStat
+	{
+		Strength,
+		Dexterity,
+		Constitution,
+		Intelligence,
+		Wisdom,
+		Charisma
+	}
+
+	struct Skill
+	{
+		public string Name { get; private set; }
+		public int Bonus { get; private set; }
+		public readonly MainStat MainStatType;
+		public bool IsProficient { get; private set; }
+
+		public Skill(string name, MainStat mainStatType, int mainStat, int proficiencyBonus, bool isProficient)
+		{
+			Name = name;
+			MainStatType = mainStatType;
+			Bonus = 0;
+			IsProficient = isProficient;
+			CalculateBonus(mainStat, proficiencyBonus);
+		}
+
+		public void CalculateBonus(int mainStat, int proficiencyBonus)
+		{
+			Bonus = 0;
+			Bonus += Utility.CalculateMainStatBonus(mainStat);
+			if (IsProficient)
+			{
+				Bonus += proficiencyBonus;
+			}
+		}
+
+		public void ChangeProficiency(bool isProf)
+		{
+			IsProficient = isProf;
+		}
+	}
+
+	public static class Utility
+	{
+
+		public static readonly Dictionary<MainStat, string> shorthandStatString = new Dictionary<MainStat, string>
+		{
+			{MainStat.Strength, "STR" },
+			{MainStat.Dexterity, "DEX" },
+			{MainStat.Constitution, "CON" },
+			{MainStat.Intelligence, "INT" },
+			{MainStat.Wisdom, "WIS" },
+			{MainStat.Charisma, "CHA" }
+		};
+		public static int CalculateMainStatBonus(int mainStat)
+		{
+			int bonus = 0;
+			mainStat -= 10;
+			bonus /= 2;
+			return bonus;
+		}
+	}
+}
+
--- DnD-Character-Manager/Model/5ECharacterModel.cs
+++ DnD-Character-Manager/Model/5ECharacterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,21 @@ namespace DnD_Character_Manager.Model
 {
 	class CharacterModel5E
 	{
+		//Stored as the full number, the Bonus will be calculated on the fly
+		public Dictionary<MainStat, int> mainstats = new Dictionary<MainStat, int>
+		{
+			{ MainStat.Strength, 0},
+			{ MainStat.Dexterity, 0 },
+			{ MainStat.Constitution, 0 },
+			{ MainStat.Intelligence, 0 },
+			{ MainStat.Wisdom, 0 },
+			{ MainStat.Charisma, 0 }
+
+		};
+
+		private ObservableCollection<Tuple<string, int, MainStat>> skills = new ObservableCollection<Tuple<string, int, MainStat>>();
+
+		public ObservableCollection<Tuple<string, int, MainStat>> Skills { get { return skills; } }
 		
 	}
 }
