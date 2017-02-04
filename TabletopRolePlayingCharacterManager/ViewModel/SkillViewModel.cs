de06429a188bd4dc1fc5e1443325f8b5aae59572
Scott Daniels <archerofyail@live.com>
2017-02-03 20:29:58 -0700

    Create skill viewmodel

---
+++ TabletopRolePlayingCharacterManager/ViewModel/SkillViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabletopRolePlayingCharacterManager.ViewModel
+{
+	class SkillViewModel : ViewModelBase
+	{
+		public string Name { get; set; }
+		public string Level { get; set; }
+		public bool IsProficient { get; set; }
+	}
+}
